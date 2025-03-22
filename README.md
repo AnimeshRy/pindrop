0a99a7a9486b768d36d6800fb6113cc8b13e56c2
AnimeshRy <ani16september@gmail.com>
2025-03-22 11:05:46 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# On Call Bot
+
+This bot tries to answer your client calls over email and discord
---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pindrop!")
+
+
+if __name__ == "__main__":
+    main()
